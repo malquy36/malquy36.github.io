fbfd07e14668062eefdcdd97326ecd39b6f97fad
Malcom Romero <malcom.romero@gmail.com>
2024-05-27 01:50:14 -0300

    últimas modificaciones y suma de modificaciones Daniela
    • Traté de sumar los cambios de la rama Daniela manualmente, sacando algunos que se pisaban.
    • Las fotos dejé las que estaban por un tema de practicidad solamente.
    • Agregué la estructura de carpetas mas ordenada junto con el tema de los script para el clima.
    • Modifiqué el estilo del formulario por uno responsive porque el que estaba en tamaño celular se rompía el body y quedaba corrido el footer junto con la imagen de fondo, ahora se agrandan hasta cierto tamaño y cambian directamente cuando pasan a resolución de "celular medio"

---
+++ static/js/climaAPI.js
@@ -0,0 +1,20 @@
+function obtenerClima(location) {
+    location = location.replace(' ','%20');
+    // Hacer una solicitud a la API de clima (sustituye 'API_KEY' con tu propia clave)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=0f74f5f77543aa793dec16603ef48b81&lang=es&units=metric`)
+        .then(response => response.json())
+        .then(data => {
+            // Mostrar la información del clima en el div 'clima-info'
+            document.getElementById('clima-info').innerHTML = `
+                <p><i class="fas fa-temperature-high"></i><span class="temperature"> Sensación Térmica: ${data.main.feels_like}°C</span></p>
+                <p><i class="fas fa-wind"></i>Viento: ${data.wind.speed} m/s</p>
+                <p><i class="fas fa-cloud"></i>Tiempo Ahora: ${data.weather[0].description}</p>
+                <p><i class="fas fa-tint"></i>Humedad: ${data.main.humidity}%</p>
+            `;
+        })
+        .catch(error => console.log('Error al obtener el clima', error));
+}
+
+// window.onload = function() {
+//     obtenerClima();
+// };
--- static/js/contact.js
+++ static/js/contact.js
@@ -1,103 +1,62 @@
 function validarFormulario() {
+    let valor;
+    let elemento;
 
     valor = document.getElementById("nombre").value;
-            
-    if( valor == null || valor.length == 0) 
-    {
-    
+    if (valor == null || valor.length == 0) {
         document.getElementById('msgErrorNombre').innerHTML = "¡Tenes que completar el nombre!";
         elemento = document.getElementById('nombre');
         elemento.focus();
-    
         return false;
     }
     document.getElementById('msgErrorNombre').innerHTML = "";
 
     valor = document.getElementById("telefono").value;
-        
-        if( isNaN(valor) || valor.length<=9) 
-        {
-            document.getElementById('msgErrorTelefono').innerHTML = "¡Ingresá un celular válido!";
-            elemento = document.getElementById('telefono');
-            elemento.value="";
-            elemento.focus();
-
-            return false;
-        }
+    if (isNaN(valor) || valor.length <= 9) {
+        document.getElementById('msgErrorTelefono').innerHTML = "¡Ingresá un celular válido!";
+        elemento = document.getElementById('telefono');
+        elemento.value = "";
+        elemento.focus();
+        return false;
+    }
     document.getElementById('msgErrorTelefono').innerHTML = "";
 
-    valor = document.getElementById("email").value;         
-    if (!(/^[^@]+@[^@\.]+\.[^@]+$/.test(valor)))
-    {
-        document.getElementById('msgErrorEmail').innerHTML = "¡Ingresá un email válido!" ;
-        
+    valor = document.getElementById("email").value;
+    if (!(/^[^@]+@[^@\.]+\.[^@]+$/.test(valor))) {
+        document.getElementById('msgErrorEmail').innerHTML = "¡Ingresá un email válido!";
         elemento = document.getElementById('email');
         elemento.value = '';
         elemento.focus();
-
         return false;
     }
     document.getElementById('msgErrorEmail').innerHTML = "";
 
-    valor = document.getElementById("motivo").value; 
-    if( valor == null || valor.length == 0) 
-    {
-    
+    valor = document.getElementById("motivo").value;
+    if (valor == null || valor.length == 0) {
         document.getElementById('msgErrorMotivo').innerHTML = "Por favor, seleccioná el motivo del contacto.";
         elemento = document.getElementById('motivo');
         elemento.focus();
-    
         return false;
     }
     document.getElementById('msgErrorMotivo').innerHTML = "";
 
     valor = document.getElementById("mensaje").value;
-    
-    if( valor == null || valor.length == 0) 
-    {
-    
+    if (valor == null || valor.length == 0) {
         document.getElementById('msgErrorComentario').innerHTML = "¡Dejanos tu mensaje!";
         elemento = document.getElementById('mensaje');
         elemento.focus();
-    
         return false;
     }
     document.getElementById('msgErrorComentario').innerHTML = "";
-    
-    return true;          
+
+    // Mostrar mensaje de agradecimiento y limpiar formulario
+    document.getElementById('alertContainer').innerHTML = [
+        `<div class="alert alert-success alert-dismissible" role="alert">`,
+        `   <div>¡Gracias por contactarnos! Responderemos a la brevedad.</div>`,
+        '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+        '</div>'
+    ].join('');
+    document.getElementById("formulario").reset();
+
+    return false;
 }
- 
-
-    //     debugger;
-//     let nombre = document.getElementById('nombre').value;
-//     let email = document.getElementById('email').value;
-//     let telefono = document.getElementById('telefono').value;
-//     let motivo = document.getElementById('motivo').value;
-//     let mensaje = document.getElementById('mensaje').value;
-//     let isValid = true;
-//     if (nombre.trim() == '') {
-//         document.getElementById("msgErrorNombre").style.display = "inline";
-//         isValid = false;
-//     }
-
-//     if (email.trim() == '') {
-//         alert('Por favor, ingresa tu correo electrónico.');
-//         return false;
-//     }
-
-//     if (telefono.trim() == '') {
-//         alert('Por favor, ingresa tu número de teléfono.');
-//         return false;
-//     }
-
-//     if (motivo.trim() == '') {
-//         alert('Por favor, selecciona un motivo de contacto.');
-//         return false;
-//     }
-
-//     if (mensaje.trim() == '') {
-//         alert('Por favor, ingresa tu mensaje.');
-//         return false;
-//     }
-
-//     return isValid;
